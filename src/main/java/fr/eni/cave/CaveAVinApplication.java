f191f8537465abc94ba63e6c677940ff645aad86
robin <robin.l79@hotmail.fr>
2023-02-01 11:54:07 +0100

    Initial commit

---
+++ src/main/java/fr/eni/cave/CaveAVinApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.cave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaveAVinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaveAVinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/cave/CaveAVinApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.cave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaveAVinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
